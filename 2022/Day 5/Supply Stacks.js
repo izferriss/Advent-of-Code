c724d397c99ac5130830f7672518d9a3d413b550
Ivan Ferriss <ferrissi@gmail.com>
2022-12-05 11:05:02 -0500

    day 5 complete

--- 2022/Day 5/Supply Stacks.js
+++ 2022/Day 5/Supply Stacks.js
@@ -49,6 +49,50 @@
 
 // After the rearrangement procedure completes, what crate ends up on top of each stack?
 
+// --- Part Two ---
+// As you watch the crane operator expertly rearrange the crates, you notice the process isn't following your prediction.
+
+// Some mud was covering the writing on the side of the crane, and you quickly wipe it away. The crane isn't a CrateMover 9000 - it's a CrateMover 9001.
+
+// The CrateMover 9001 is notable for many new and exciting features: air conditioning, leather seats, an extra cup holder, and the ability to pick up and move multiple crates at once.
+
+// Again considering the example above, the crates begin in the same configuration:
+
+//     [D]    
+// [N] [C]    
+// [Z] [M] [P]
+//  1   2   3 
+// Moving a single crate from stack 2 to stack 1 behaves the same as before:
+
+// [D]        
+// [N] [C]    
+// [Z] [M] [P]
+//  1   2   3 
+// However, the action of moving three crates from stack 1 to stack 3 means that those three moved crates stay in the same order, resulting in this new configuration:
+
+//         [D]
+//         [N]
+//     [C] [Z]
+//     [M] [P]
+//  1   2   3
+// Next, as both crates are moved from stack 2 to stack 1, they retain their order as well:
+
+//         [D]
+//         [N]
+// [C]     [Z]
+// [M]     [P]
+//  1   2   3
+// Finally, a single crate is still moved from stack 1 to stack 2, but now it's crate C that gets moved:
+
+//         [D]
+//         [N]
+//         [Z]
+// [M] [C] [P]
+//  1   2   3
+// In this example, the CrateMover 9001 has put the crates in a totally different order: MCD.
+
+// Before the rearrangement process finishes, update your simulation so that the Elves know where they should stand to be ready to unload the final supplies. After the rearrangement procedure completes, what crate ends up on top of each stack?
+
 let stacks = 
     [
         ['W', 'R', 'T'],
@@ -584,13 +628,31 @@ for(var i = 0; i < input.length; i++)
     processInstruction(move, from, to);
 }
 
+//PART 1
 //Do work
+// function processInstruction(move, from, to)
+// {
+//     while(move > 0)
+//     {
+//         stacks[to - 1].push(stacks[from - 1].pop());
+//         move--;
+//     }
+// }
+
+//PART2
 function processInstruction(move, from, to)
 {
-    console.log("move " + move + " from " + from + " to " + to);
+    let intermediateStack = [];
+    let temp = move;
     while(move > 0)
     {
-        stacks[to - 1].push(stacks[from - 1].pop());
+        intermediateStack.push(stacks[from - 1].pop());
+        move--;
+    }
+    move = temp;
+    while(move > 0)
+    {
+        stacks[to - 1].push(intermediateStack.pop());
         move--;
     }
 }
