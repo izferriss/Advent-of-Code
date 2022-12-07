b03b2cf62f65f769948ae35af220b6f593e4ea1d
Ivan <94561520+izferriss@users.noreply.github.com>
2022-12-07 18:39:41 +0000

    part2 complete

--- 2022/Day 7/No Space Left On Device.js
+++ 2022/Day 7/No Space Left On Device.js
@@ -73,6 +73,23 @@
 
 // Find all of the directories with a total size of at most 100000. What is the sum of the total sizes of those directories?
 
+// --- Part Two ---
+// Now, you're ready to choose a directory to delete.
+
+// The total disk space available to the filesystem is 70000000. To run the update, you need unused space of at least 30000000. You need to find a directory you can delete that will free up enough space to run the update.
+
+// In the example above, the total size of the outermost directory (and thus the total amount of used space) is 48381165; this means that the size of the unused space must currently be 21618835, which isn't quite the 30000000 required by the update. Therefore, the update still requires a directory with total size of at least 8381165 to be deleted before it can run.
+
+// To achieve this, you have the following options:
+
+// Delete directory e, which would increase unused space by 584.
+// Delete directory a, which would increase unused space by 94853.
+// Delete directory d, which would increase unused space by 24933642.
+// Delete directory /, which would increase unused space by 48381165.
+// Directories e and a are both too small; deleting them would not free up enough space. However, directories d and / are both big enough! Between these, choose the smallest: d, increasing unused space by 24933642.
+
+// Find the smallest directory that, if deleted, would free up enough space on the filesystem to run the update. What is the total size of that directory?
+
 let input =
     [
         "$ cd /",
@@ -1140,6 +1157,7 @@ let input =
         "60923 pldhhjch"
     ];
 
+//PART 1
 const root = "root";
 let sumFileSize = 0;
 let folders = {};
@@ -1203,9 +1221,6 @@ for(var i = 0; i < input.length; i++)
     }
 }
 
-console.log(sumFileSize);
-console.log(folders);
-
 //Filter on folder values (sizes) for files less than 100000 in size
 let sub100kFolders = Object.values(folders).filter(files => files <= 100000);
 let sumTotal = 0;
@@ -1217,3 +1232,22 @@ for(var i = 0; i < sub100kFolders.length; i++)
 }
 
 console.log(sumTotal);
+
+//PART 2
+
+//Current usage of disk space
+const currentDiskSpace = folders[root];
+
+//Total available disk space
+const availableDiskSpace = 70000000;
+
+//Amount of disk space required to install update
+const requiredDiskSpace = 30000000;
+
+//Filter on folder values (sizes) for files that cannot fit on the disk space with the new update in mind
+const potentialDeletions = Object.values(folders).filter(files => files >= (currentDiskSpace + requiredDiskSpace - availableDiskSpace));
+
+//Find the smallest value by spreading and comparing deletion candidates
+let smallestDeletion = Math.min(...potentialDeletions);
+
+console.log(smallestDeletion);
