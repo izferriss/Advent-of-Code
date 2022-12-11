bcd0fd955e9382e70d5101eaeb6bc8975a79b526
izferriss <ferrissi@gmail.com>
2022-12-11 15:37:51 -0500

    Add files via Visual Studio Code

--- 2022/Day 11/Monkey in the Middle.js
+++ 2022/Day 11/Monkey in the Middle.js
@@ -210,6 +210,90 @@
 
 // Figure out which monkeys to chase by counting how many items they inspect over 20 rounds. What is the level of monkey business after 20 rounds of stuff-slinging simian shenanigans?
 
+// --- Part Two ---
+// You're worried you might not ever get your items back. So worried, in fact, that your relief that a monkey's inspection didn't damage an item no longer causes your worry level to be divided by three.
+
+// Unfortunately, that relief was all that was keeping your worry levels from reaching ridiculous levels. You'll need to find another way to keep your worry levels manageable.
+
+// At this rate, you might be putting up with these monkeys for a very long time - possibly 10000 rounds!
+
+// With these new rules, you can still figure out the monkey business after 10000 rounds. Using the same example above:
+
+// == After round 1 ==
+// Monkey 0 inspected items 2 times.
+// Monkey 1 inspected items 4 times.
+// Monkey 2 inspected items 3 times.
+// Monkey 3 inspected items 6 times.
+
+// == After round 20 ==
+// Monkey 0 inspected items 99 times.
+// Monkey 1 inspected items 97 times.
+// Monkey 2 inspected items 8 times.
+// Monkey 3 inspected items 103 times.
+
+// == After round 1000 ==
+// Monkey 0 inspected items 5204 times.
+// Monkey 1 inspected items 4792 times.
+// Monkey 2 inspected items 199 times.
+// Monkey 3 inspected items 5192 times.
+
+// == After round 2000 ==
+// Monkey 0 inspected items 10419 times.
+// Monkey 1 inspected items 9577 times.
+// Monkey 2 inspected items 392 times.
+// Monkey 3 inspected items 10391 times.
+
+// == After round 3000 ==
+// Monkey 0 inspected items 15638 times.
+// Monkey 1 inspected items 14358 times.
+// Monkey 2 inspected items 587 times.
+// Monkey 3 inspected items 15593 times.
+
+// == After round 4000 ==
+// Monkey 0 inspected items 20858 times.
+// Monkey 1 inspected items 19138 times.
+// Monkey 2 inspected items 780 times.
+// Monkey 3 inspected items 20797 times.
+
+// == After round 5000 ==
+// Monkey 0 inspected items 26075 times.
+// Monkey 1 inspected items 23921 times.
+// Monkey 2 inspected items 974 times.
+// Monkey 3 inspected items 26000 times.
+
+// == After round 6000 ==
+// Monkey 0 inspected items 31294 times.
+// Monkey 1 inspected items 28702 times.
+// Monkey 2 inspected items 1165 times.
+// Monkey 3 inspected items 31204 times.
+
+// == After round 7000 ==
+// Monkey 0 inspected items 36508 times.
+// Monkey 1 inspected items 33488 times.
+// Monkey 2 inspected items 1360 times.
+// Monkey 3 inspected items 36400 times.
+
+// == After round 8000 ==
+// Monkey 0 inspected items 41728 times.
+// Monkey 1 inspected items 38268 times.
+// Monkey 2 inspected items 1553 times.
+// Monkey 3 inspected items 41606 times.
+
+// == After round 9000 ==
+// Monkey 0 inspected items 46945 times.
+// Monkey 1 inspected items 43051 times.
+// Monkey 2 inspected items 1746 times.
+// Monkey 3 inspected items 46807 times.
+
+// == After round 10000 ==
+// Monkey 0 inspected items 52166 times.
+// Monkey 1 inspected items 47830 times.
+// Monkey 2 inspected items 1938 times.
+// Monkey 3 inspected items 52013 times.
+// After 10000 rounds, the two most active monkeys inspected items 52166 and 52013 times. Multiplying these together, the level of monkey business in this situation is now 2713310158.
+
+// Worry levels are no longer divided by three after each item is inspected; you'll need to find another way to keep your worry levels manageable. Starting again from the initial state in your puzzle input, what is the level of monkey business after 10000 rounds?
+
 let input =
 [
     [//Monkey 0
@@ -288,26 +372,68 @@ let input =
 
 let currentRound = 1;
 let inspectedItemsPerMonkey = [0, 0, 0, 0, 0, 0, 0, 0];
-while(currentRound <= 20)
+
+// PART 1
+// let numRounds = 20;
+
+// PART 2
+let numRounds = 10000;
+
+let divisor = 1;
+
+//product of divisors
+for(var i = 0; i < input.length; i++)
+{
+    divisor *= input[i][0].test;
+}
+
+while(currentRound <= numRounds)
 {
     for(var i = 0; i < input.length; i++)
     {
-        while(input[i][0].items.length > 0)
+        while(input[i][0].items.length)
         {
             //Check first item
             let item = input[i][0].items.shift();
-            console.log("shifted: " + item);
 
             //Increment that an item has been checked
             inspectedItemsPerMonkey[i]++;
             
             //Increase item's worry level
-            item = eval(input[i][0].operation.replaceAll("old", item));
-            console.log("operated: " + item);
+            switch (i)
+            {
+                case 0:
+                    item = item * 11;
+                    break;
+                case 1:
+                    item = item + 1;
+                    break;
+                case 2:
+                    item = item + 7;
+                    break;
+                case 3:
+                    item = item + 3;
+                    break;
+                case 4:
+                    item = item * item;
+                    break;
+                case 5:
+                    item = item + 4;
+                    break;
+                case 6:
+                    item = item * 5;
+                    break;
+                case 7:
+                    item = item + 8;
+                    break;
+            }
 
-            //Reduce item's worry level
-            item = Math.floor(item / 3);
-            console.log("div3: " + item);
+            //item's remainder after dividing the product of divisors will keep item's value under divisor's value while still being mathematically logical
+            item = item % divisor;
+
+            // PART 1
+            // Reduce item's worry level
+            // item = Math.floor(item / 3);
 
             //Test item to pass to next monkey
             if(item % input[i][0].test == 0)
@@ -324,7 +450,6 @@ while(currentRound <= 20)
     currentRound++;
 }
 
-console.log(input);
-inspectedItemsPerMonkey.sort(function(a, b) {return a - b;});
-console.log(inspectedItemsPerMonkey.reverse());
+inspectedItemsPerMonkey.sort((a,b) => b - a);
+console.log(inspectedItemsPerMonkey);
 console.log(inspectedItemsPerMonkey[0] * inspectedItemsPerMonkey[1]);
